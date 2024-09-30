ca778d93a02e7d7991523e0c81d59c862fd9ae43
claudioaltamura <claudio@claudioaltamura.de>
2024-09-30 10:31:47 +0200

    feat: test with thresholds

---
+++ performance-test.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { BASEURL } from './config.js';
+
+export const options = {
+  vus: 1,
+  duration: '10s',
+  thresholds: {
+    http_req_duration: ['p(95)<100'],
+    http_req_failed: ['rate<0.01']
+  }
+};
+
+export default function () {
+  const res = http.get(BASEURL + '/pets?tags=cat&limit=10');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'result is empty': (r) => r.body === '[]'
+  });
+}
