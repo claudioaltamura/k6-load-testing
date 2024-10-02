efa69769e1900e3ff034487eddb1ba8e4de3ef4f
claudioaltamura <claudio@claudioaltamura.de>
2024-10-02 10:47:47 +0200

    feat: custom metric

--- performance-test.js
+++ performance-test.js
@@ -1,19 +1,22 @@
 import http from 'k6/http';
 import { check } from 'k6';
+import { Trend } from 'k6/metrics';
 import { BASEURL } from './config.js';
 
 export const options = {
   vus: 1,
   duration: '10s',
   thresholds: {
-    http_req_duration: ['p(95)<100'],
-    http_req_duration: ['max<2000'],
-    http_req_failed: ['rate<0.01']
+    list_cats_limit_10: ['p(95)<100', 'max<2000'],
+    checks: ['rate<0.01']
   }
 };
 
+let list_cats_limit_10 = new Trend('list_cats_limit_10')
+
 export default function () {
   const res = http.get(BASEURL + '/pets?tags=cat&limit=10');
+  list_cats_limit_10.add(res.timings.duration)
   check(res, {
     'status is 200': (r) => r.status === 200,
     'result is empty': (r) => r.body === '[]'
