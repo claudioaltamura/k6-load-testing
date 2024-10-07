48d5061c652fe98bfc91f23b55343e5dc3199433
claudioaltamura <claudio@claudioaltamura.de>
2024-10-07 11:30:19 +0200

    feat: add custom tags, threshold, check, request

--- performance-test.js
+++ performance-test.js
@@ -8,17 +8,26 @@ export const options = {
   duration: '10s',
   thresholds: {
     list_cats_limit_10: ['p(95)<100', 'max<200'],
-    checks: ['rate<0.01']
+    'http_req_duration{page:petlist}': ['p(95)<100'],
+    checks: ['rate<0.02'],
+    'checks{page:petlist}': ['rate<0.01']
   }
 };
 
 let list_cats_limit_10 = new Trend('list_cats_limit_10')
 
 export default function () {
-  const res = http.get(BASEURL + '/pets?tags=cat&limit=10');
+  const res = http.get(BASEURL + '/pets?tags=cat&limit=10', 
+    { tags: 
+      { page: 'petlist'}
+    } 
+  );
   list_cats_limit_10.add(res.timings.duration)
   check(res, {
     'status is 200': (r) => r.status === 200,
     'result is empty': (r) => r.body === '[]'
-  });
+    }, {
+      page: 'petlist'
+    }
+  );
 }
