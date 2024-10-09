6e605fdfd9126f222afc9230a393741fc9cf9b4f
claudioaltamura <claudio@claudioaltamura.de>
2024-10-09 15:02:58 +0200

    feat: add  counter and tags

--- performance-test.js
+++ performance-test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
-import { Trend } from 'k6/metrics';
+import { Counter, Trend } from 'k6/metrics';
 import { BASEURL } from './config.js';
 
 export const options = {
@@ -9,12 +9,15 @@ export const options = {
   thresholds: {
     list_cats_limit_10: ['p(95)<100', 'max<200'],
     'http_req_duration{page:petlist}': ['p(95)<100'],
+    'http_errors': ['count==0'],
+    'http_errors{page:petlist}': ['count==0'],
     checks: ['rate<0.02'],
     'checks{page:petlist}': ['rate<0.01']
   }
 };
 
-let list_cats_limit_10 = new Trend('list_cats_limit_10')
+let list_cats_limit_10 = new Trend('list_cats_limit_10');
+let httpErrors = new Counter("http_errors");
 
 export default function () {
   const res = http.get(BASEURL + '/pets?tags=cat&limit=10', 
@@ -22,7 +25,15 @@ export default function () {
       { page: 'petlist'}
     } 
   );
+  
+  if(res.error) {
+    httpErrors.add(1, {
+      page: 'petlist'
+    });
+  }
+
   list_cats_limit_10.add(res.timings.duration)
+
   check(res, {
     'status is 200': (r) => r.status === 200,
     'result is empty': (r) => r.body === '[]'
